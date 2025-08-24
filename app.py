89093f9a03a7cb37295f3d2d7bb453b2d280a81f
Michal Mordarski <mmordarski4@gmail.com>
2025-08-24 11:14:05 +0200

    Add REST API

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort, make_response
 
 from forms import TableForm
 from models import tables
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nininini"
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
+
+@app.route('/api/v1/tables', methods=['GET'])
+def tables_list_api_v1():
+    return jsonify(tables.all())
+
+@app.route('/api/v1/tables/<int:table_id>', methods=['GET'])
+def get_table(table_id):
+    table = tables.get(table_id)
+    if not table:
+        abort(404)
+    return jsonify({"table": table})
+
+@app.route('/api/v1/tables/', methods=['POST'])
+def create_table():
+    if not request.json or not 'guests' in request.json:
+        abort(400)
+    table = {
+        'table_num': tables.all()[-1]['table_num'] + 1,
+        'guests': request.json['guests']
+    }
+    tables.create(table)
+    return jsonify({'table': table}), 201
+
+@app.route('/api/v1/tables/<int:table_id>', methods=['DELETE'])
+def delete_table(table_id):
+    result = tables.delete(table_id)
+    if not result:
+        abort(404)
+    return jsonify({'result': result})
+
+@app.route('/api/v1/tables/<int:table_id>', methods=['PUT'])
+def update_table(table_id):
+    table = tables.get(table_id)
+    if not table:
+        abort(404)
+    if not request.json:
+        abort(400)
+    data = request.json
+    if 'guests' in data and not isinstance(data.get('guests'), str):
+        abort(400)
+    table['guests'] = data.get('guests', table['guests'])
+    tables.update(table_id, table)
+    return jsonify({'table': table})
+
+        
 
 @app.route('/tables/', methods=['GET', 'POST'])
 def tables_list():
@@ -20,12 +73,12 @@ def tables_list():
 
 @app.route('/tables/<int:table_num>', methods=['GET', 'POST'])
 def table_details(table_num):
-    table = tables.get(table_num-1)
+    table = tables.get(table_num)
     form = TableForm(data=table)
 
     if request.method == 'POST':
         if form.validate_on_submit():
-            tables.update(table_num-1, form.data)
+            tables.update(table_num, form.data)
         return redirect(url_for("tables_list"))
     return render_template("table.html", form=form, table_num=table_num)
 
--- models.py
+++ models.py
@@ -11,20 +11,37 @@ class Tables:
     def all(self):
         return self.tables
     
-    def get(self, num):
-        return self.tables[num]
+    def get(self, table_num):
+        for table in self.tables:
+            if table['table_num'] == table_num:
+                return table
+        return []
 
     def create(self, data):
-        data.pop('csrf_token')
+        data.pop('csrf_token', None)
         self.tables.append(data)
+        self.save_all()
 
     def save_all(self):
         with open('tables.json', 'w') as f:
             json.dump(self.tables,f)
 
-    def update(self, num, data):
-        data.pop('csrf_token')
-        self.tables[num] = data
-        self.save_all()
+    def update(self, table_num, data):
+        data.pop('csrf_token', None)
+        table = self.get(table_num)
+        if table:
+            index = self.tables.index(table)
+            self.tables[index] = data
+            self.save_all()
+            return True
+        return False
+
+    def delete(self, table_num):
+        todo = self.get(table_num)
+        if todo:
+            self.tables.remove(todo)
+            self.save_all()
+            return True
+        return False
 
 tables = Tables()
